ea967d1b09d01ce1e675ee78ef354ac7d4d67b1d
TangSong <502091250@qq.com>
2017-12-15 18:47:52 +0800

    circle view: add wrap_content feature

--- OverrideOnDraw/app/src/main/java/com/example/overrideondraw/wedgit/CircleView.java
+++ OverrideOnDraw/app/src/main/java/com/example/overrideondraw/wedgit/CircleView.java
@@ -53,4 +53,24 @@ public class CircleView extends View {
         int radius = Math.min(width, height)/2;
         canvas.drawCircle(pdl + width/2, pdt + height/2, radius, mPaint);
     }
+
+    // 重写onMeasure()函数目的：处理xml文件中wrap_content属性。否则wrap_content表现出match_parent的效果
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int wSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int wSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int hSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int hSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        // 对于wrap_content的情况，我们指定一个默认大小
+        if (wSpecMode == MeasureSpec.AT_MOST && hSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(400, 400);
+        } else if (wSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(400, hSpecSize);
+        } else if (hSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(wSpecSize, 400);
+        }
+        // width和height都不是wrap_content的情况时，使用传入参数作为大小。
+    }
 }
